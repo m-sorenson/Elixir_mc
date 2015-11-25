09850d41732a318e19af9b9beb20668f69c7f4c1
Michael Sorenson <m.sorenson407@gmail.com>
2015-11-24 22:28:41 -0700

    Start task_sup and server. Added functions for creating new workers.

--- lib/missionaries_cannibals.ex
+++ lib/missionaries_cannibals.ex
@@ -1,6 +1,9 @@
 defmodule MC do
   use Application
 
+  @server_name MC.Server
+  @task_sup MC.TaskSup
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -9,11 +12,21 @@ defmodule MC do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(MC.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: @task_sup]]),
+      worker(MC.Server, [[name: @server_name]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MC.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def create_worker(state) do
+    Task.Supervisor.start_child(@task_sup, fn -> MC.TaskWorker.start(state) end)
+  end
+
+  def start_state() do
+    create_worker([{1, 3, 3}])
   end
 end
