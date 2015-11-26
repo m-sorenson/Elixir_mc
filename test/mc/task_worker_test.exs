68e7058faa3952b73905411d39a34154f368f92b
Michael Sorenson <m.sorenson407@gmail.com>
2015-11-26 00:54:30 -0700

    Added tests for duplicate function.

--- test/mc/task_worker_test.exs
+++ test/mc/task_worker_test.exs
@@ -36,4 +36,35 @@ defmodule MC.TaskWorkerTest do
   test "found goal" do
     assert true == MC.TaskWorker.goal?({-1, 0, 0})
   end
+
+  test "no duplicates" do
+    :ets.delete_all_objects(:duplicate_check)
+    assert false == MC.TaskWorker.duplicate?({1, 3, 3})
+    assert false == MC.TaskWorker.duplicate?({1, 2, 3})
+    assert false == MC.TaskWorker.duplicate?({1, 2, 0})
+    assert false == MC.TaskWorker.duplicate?({1, 0, 0})
+    assert false == MC.TaskWorker.duplicate?({1, 1, 1})
+    assert false == MC.TaskWorker.duplicate?({-1, 0, 0})
+    assert false == MC.TaskWorker.duplicate?({-1, 3, 2})
+    assert false == MC.TaskWorker.duplicate?({-1, 1, 2})
+  end
+
+  test "all duplicates" do
+    MC.TaskWorker.duplicate?({1, 3, 3})
+    assert true == MC.TaskWorker.duplicate?({1, 3, 3})
+    MC.TaskWorker.duplicate?({1, 2, 3})
+    assert true == MC.TaskWorker.duplicate?({1, 2, 3})
+    MC.TaskWorker.duplicate?({1, 2, 0})
+    assert true == MC.TaskWorker.duplicate?({1, 2, 0})
+    MC.TaskWorker.duplicate?({1, 0, 0})
+    assert true == MC.TaskWorker.duplicate?({1, 0, 0})
+    MC.TaskWorker.duplicate?({1, 1, 1})
+    assert true == MC.TaskWorker.duplicate?({1, 1, 1})
+    MC.TaskWorker.duplicate?({-1, 0, 0})
+    assert true == MC.TaskWorker.duplicate?({-1, 0, 0})
+    MC.TaskWorker.duplicate?({-1, 3, 2})
+    assert true == MC.TaskWorker.duplicate?({-1, 3, 2})
+    MC.TaskWorker.duplicate?({-1, 1, 2})
+    assert true == MC.TaskWorker.duplicate?({-1, 1, 2})
+  end
 end
