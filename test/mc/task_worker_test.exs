a6069ec653ec55ebedcf0eab34ed94fdce6de937
Michael Sorenson <m.sorenson407@gmail.com>
2015-12-07 09:29:47 -0700

    Changed parameters of goal. Bug with server dying during duplicate test.

--- test/mc/task_worker_test.exs
+++ test/mc/task_worker_test.exs
@@ -1,5 +1,5 @@
 defmodule MC.TaskWorkerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   test "invalid states tests" do
     assert false == MC.TaskWorker.valid?({1, 2, 3})
@@ -26,15 +26,17 @@ defmodule MC.TaskWorkerTest do
   end
 
   test "not goal states" do
-    assert false == MC.TaskWorker.goal?({1, 3, 3})
-    assert false == MC.TaskWorker.goal?({1, 0, 0})
-    assert false == MC.TaskWorker.goal?({-1, 3, 3})
-    assert false == MC.TaskWorker.goal?({-1, 1, 2})
-    assert false == MC.TaskWorker.goal?({-1, 0, 1})
+    assert false == MC.TaskWorker.goal?([{1, 3, 3}])
+    assert false == MC.TaskWorker.goal?([{1, 0, 0}])
+    assert false == MC.TaskWorker.goal?([{-1, 3, 3}])
+    assert false == MC.TaskWorker.goal?([{-1, 1, 2}])
+    assert false == MC.TaskWorker.goal?([{-1, 0, 1}])
   end
 
   test "found goal" do
-    assert true == MC.TaskWorker.goal?({-1, 0, 0})
+    assert true == MC.TaskWorker.goal?([{-1, 0, 0}])
+    assert true == MC.TaskWorker.goal?([{-1, 0, 0}, {1, 2, 3}])
+    assert true == MC.TaskWorker.goal?([{-1, 0, 0}, {1, 2, 3}, {1, 1, 1}])
   end
 
   test "no duplicates" do
