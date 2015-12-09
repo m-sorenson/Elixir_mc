2f800082b4a2ab4ba7d4c965567e1ac13fa8d0d0
Pablo Trujillo <ptrujill@dmail.dixie.edu>
2015-12-09 02:53:34 -0700

    implement valid?() and safe?() functions in MC.TaskWorker module, add tests for safe?()

--- lib/mc/task_worker.ex
+++ lib/mc/task_worker.ex
@@ -17,9 +17,24 @@ defmodule MC.TaskWorker do
   @doc """
   Takes a state and returns a boolean declaring if the state is valid.
   """
-  def valid?(_) do
-    false
+  def valid?(state = {_, m, c}) do
+		if safe?(state) and m >= 0 and m <= 3 and c >= 0 and c <= 3 do
+			true
+		else
+			false
+		end
   end
+
+	@doc """
+	Takes a state and returns a boolean declaring if the state is safe.
+	"""
+	def safe?({_, m, c}) do
+		if m == 0 or m == 3 or m == c do
+			true
+		else
+			false
+		end
+	end
 
   @doc """
   Tests if state is goal
--- test/mc/task_worker_test.exs
+++ test/mc/task_worker_test.exs
@@ -25,6 +25,28 @@ defmodule MC.TaskWorkerTest do
     assert true == MC.TaskWorker.valid?({1, 0, 1})
   end
 
+	test "unsafe states tests" do
+    assert false == MC.TaskWorker.safe?({1, 1, 3})
+		assert false == MC.TaskWorker.safe?({1, 1, 2})
+		assert false == MC.TaskWorker.safe?({1, 1, 0})
+    assert false == MC.TaskWorker.safe?({1, 2, 3})
+		assert false == MC.TaskWorker.safe?({1, 2, 1})
+		assert false == MC.TaskWorker.safe?({1, 2, 0})
+  end
+
+  test "safe states tests" do
+    assert true == MC.TaskWorker.safe?({1, 0, 3})
+    assert true == MC.TaskWorker.safe?({1, 0, 2})
+    assert true == MC.TaskWorker.safe?({1, 0, 1})
+		assert true == MC.TaskWorker.safe?({1, 0, 0})
+    assert true == MC.TaskWorker.safe?({1, 1, 1})
+    assert true == MC.TaskWorker.safe?({1, 2, 2})
+    assert true == MC.TaskWorker.safe?({1, 3, 3})
+    assert true == MC.TaskWorker.safe?({1, 3, 2})
+    assert true == MC.TaskWorker.safe?({1, 3, 1})
+		assert true == MC.TaskWorker.safe?({1, 3, 0})
+  end
+
   test "not goal states" do
     assert false == MC.TaskWorker.goal?([{1, 3, 3}])
     assert false == MC.TaskWorker.goal?([{1, 0, 0}])
