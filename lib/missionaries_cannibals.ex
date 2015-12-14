aed97b9aa647e766b0560357279405ea36785fec
Michael Sorenson <m.sorenson407@gmail.com>
2015-12-14 11:15:03 -0700

    Changed to one_for_all.

--- lib/missionaries_cannibals.ex
+++ lib/missionaries_cannibals.ex
@@ -18,7 +18,7 @@ defmodule MC do
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: MC.Supervisor]
+    opts = [strategy: :one_for_all, name: MC.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
